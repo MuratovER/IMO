8080c2a655ae1e39acbcdb51929be116a1f5ff4d
Eldar <MuratovER@stud.kai.ru>
2022-04-03 18:44:10 +0300

    Prepared project for deploy

--- imo/settings.py
+++ imo/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -167,6 +167,6 @@ WSGI_APPLICATION = 'imo.wsgi.application'
 # DATABASES.update(default=db_from_env)
 # DATABASES['default']=db_from_env
 
-
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
