e0a937a03689358a320b836891dbb9301fcabde2
Eldar <MuratovER@stud.kai.ru>
2022-04-03 18:47:10 +0300

    Prepared project for deploy

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -1,5 +1,5 @@
-from cProfile import label
-from tkinter import Widget
+# from cProfile import label
+# from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
