02b095f470295116039808fe282bb233453b0efb
Eldar <MuratovER@stud.kai.ru>
2022-04-03 18:56:24 +0300

    wip

--- imo/settings.py
+++ imo/settings.py
@@ -73,17 +73,9 @@ TEMPLATES = [
 ]
 
 
-
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -96,16 +88,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mydb',
-#         'USER': 'myuser',
-#         'PASSWORD': 'mypass',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+
 
 WHITENOISE_USE_FINDERS = True
 
