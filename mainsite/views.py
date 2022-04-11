8397820ac6a20daa4efa134997ae4e96763cc50c
Nodir2001 <rasulovnd@stud.kai.ru>
2022-04-11 19:11:52 +0300

    register+extra

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,39 +1,65 @@
 from urllib import request
 from django.views import View
-from mainsite.forms import SignUpFormDelegate, SignUpFormStudent, SignUpForAgency
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout, login, authenticate
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from .models import *
+from .forms import  CreateUserForm
 
 def home_page(request):
     return render(request, 'mainsite/home/home_page.html')
 
+# signup view
+def signup_page(request):
+	if request.user.is_authenticated:
+		return redirect('home_page')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('extra_page')
+			
+		context = {'form':form}
+		return render(request, 'mainsite/registration/RegesterIndex.html', context)
 
 # login view
-def login_view(request):
-    return render(request, 'mainsite/registration/LoginIndex.html')
+def login_page(request):
+	if request.user.is_authenticated:
+		return redirect('home_page')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
 
+			user = authenticate(request, username=username, password=password)
 
-# signup view
-def signup_view(request):
-    '''вьюха с логикой регистрации'''
-    form = SignUpFormDelegate(request.POST)
-    if form.is_valid():
-        user = form.save()
-        user.refresh_from_db()
-        user.profile.email = form.cleaned_data.get('email')
-        user.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('user_page')
-    else:
-        form = SignUpFormDelegate()
-    return render(request, 'mainsite/registration/RegesterIndex.html', {'form': form})
+			if user is not None:
+				login(request, user)
+				return redirect('home_page')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
 
+		context = {}
+		return render(request, 'mainsite/registration/LoginIndex.html', context)
 
 # logout view
-def logout_view(request):
-    return render(request, 'mainsite/home/home_page.html')
+def logout_user(request):
+	logout(request)
+	return redirect('mainsite/home/home_page.html')    
+
+# extra-signup view
+def extra_page(request):
+	return redirect(request, 'mainsite/registration/ExtraInfo.html')
