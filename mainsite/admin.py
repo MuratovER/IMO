6a2feb9687438f12306a50c30d2ded1fdc8c8c46
Eldar <MuratovER@stud.kai.ru>
2022-05-09 17:12:17 +0300

    Added convenient admin form

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-
+from django import forms
 from mainsite.models import Profile, Post
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
+
+
+
+
+class PostAdminForm(forms.ModelForm):
+    text = forms.CharField(label="Текст поста", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Profile)
-admin.site.register(Post)
