0e5c47496c42eac138068c00a8cfe8c56e3cbf15
Eldar <58162328+MuratovER@users.noreply.github.com>
2022-05-16 21:45:33 +0300

    Update settings.py

--- imo/settings.py
+++ imo/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'imo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imodb',
+        'USER': 'imo',
+        'PASSWORD': 'imoproject',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
