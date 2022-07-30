e216e066ba4398283bcf98bbd9132c05fc23e259
MuratovER <MuratovER@stud.kai.ru>
2022-07-30 07:16:43 +0300

    Created switch logic

--- mainsite/views.py
+++ mainsite/views.py
@@ -10,153 +10,191 @@ from django.core.mail import send_mail, BadHeaderError
 from django.contrib.auth.decorators import login_required
 
 
+
 def home_page(request):
     last_4 = Post.objects.order_by()[:4]
     context = {
         'last_4': last_4
     }
-    return render(request, 'mainsite/home/home_page.html', context)
+
+    if not request.user_agent.is_mobile:
+        return render(request, 'mainsite/home/home_page.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('home_page')
+    if request.user_agent.is_mobile:
+        if request.user.is_authenticated:
+            return redirect('home_page')
+        else:
+            if request.method == 'POST':
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('home_page')
+                else:
+                    messages.info(request, 'Username OR password is incorrect')
+
+            ctx = {}
+        return render(request, 'mainsite/registration/LoginIndex.html', ctx)
     else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+        return render(request, 'mainsite/PhonePage/index.html', )
 
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home_page')
-            else:
-                messages.info(request, 'Username OR password is incorrect')
-
-        ctx = {}
-    return render(request, 'mainsite/registration/LoginIndex.html', ctx)
 
 
 
 def profile_view(request):
 
-    profile = Profile.objects.get(user=request.user)
-    if request.method == "POST":
-        form = ProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.save()
-            return redirect('profile')
-    else:
-        form = ProfileForm(instance=profile)
 
-    context = {
-        'profile': profile,
-        'form': form,
-    }
-    return render(request, 'mainsite/profile/profile.html', context)
+    if request.user_agent.is_mobile:
+        profile = Profile.objects.get(user=request.user)
+        if request.method == "POST":
+            form = ProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.save()
+                return redirect('profile')
+        else:
+            form = ProfileForm(instance=profile)
+
+        context = {
+            'profile': profile,
+            'form': form,
+        }
+        return render(request, 'mainsite/profile/profile.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
+
+
 
 
 def signup_view(request):
     '''вьюха с логикой регистрации'''
-    form = CreateUserForm(request.POST)
-    if form.is_valid():
-        user = form.save()
-        user.refresh_from_db()
-        # user.profile.email = form.cleaned_data.get('email')
-        user.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        messages.success(request, 'Account was created for ' + username)
-        login(request, user)
-        return redirect('extra')
+
+    if request.user_agent.is_mobile:
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            # user.profile.email = form.cleaned_data.get('email')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            messages.success(request, 'Account was created for ' + username)
+            login(request, user)
+            return redirect('extra')
+        else:
+            form = CreateUserForm()
+        return render(request, 'mainsite/registration/RegesterIndex.html', {'form': form})
     else:
-        form = CreateUserForm()
-    return render(request, 'mainsite/registration/RegesterIndex.html', {'form': form})
+        return render(request, 'mainsite/PhonePage/index.html', )
+
+
 
 
 def logout_view(request):
-    return render(request, 'mainsite/home/home_page.html')
+    if request.user_agent.is_mobile:
+        return render(request, 'mainsite/home/home_page.html')
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
 
 def extra_view(request):
-    try:
-        profile = request.user.profile
-    except Profile.DoesNotExist:
-        profile = Profile(user=request.user)
-    if request.method=='POST':
-        form = ProfileForm(request.POST, instance=profile)
-        
-        if form.is_valid():
-            extra = form.save()
-            return redirect('profile')
+    if request.user_agent.is_mobile:
+        try:
+            profile = request.user.profile
+        except Profile.DoesNotExist:
+            profile = Profile(user=request.user)
+        if request.method == 'POST':
+            form = ProfileForm(request.POST, instance=profile)
+
+            if form.is_valid():
+                extra = form.save()
+                return redirect('profile')
+        else:
+            form = ProfileForm(instance=profile)
+        ctx = {'form': form}
+        return render(request, 'mainsite/registration/ExtraInfo.html', ctx)
     else:
-        form = ProfileForm(instance=profile)        
-    ctx = {'form': form}
-    return render(request, 'mainsite/registration/ExtraInfo.html', ctx)
+        return render(request, 'mainsite/PhonePage/index.html', )
+
+
              
 def privacypolicy_view(request):
-    return render(request, 'mainsite/privacyPolicy/PrivacyPolicy.html')
+    if request.user_agent.is_mobile:
+        return render(request, 'mainsite/privacyPolicy/PrivacyPolicy.html')
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
     
 def enteringimo_view(request):
-    last_4 = Post.objects.order_by()[:4]
-    context = {
-        'last_4': last_4
-    }
-    return render(request, 'mainsite/enteringIMO/EnteringIMO.html', context)
+    if request.user_agent.is_mobile:
+        last_4 = Post.objects.order_by()[:4]
+        context = {
+            'last_4': last_4
+        }
+        return render(request, 'mainsite/enteringIMO/EnteringIMO.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
 def error_404_view(request, exception):
+
     return render(request, 'mainsite/404.html')
 
 def faq_view(request):
-    questions = Faq.objects.all()
-    context = {'questions': questions}
-    return render(request, 'mainsite/faq/faq.html', context)
+    if request.user_agent.is_mobile:
+        questions = Faq.objects.all()
+        context = {'questions': questions}
+        return render(request, 'mainsite/faq/faq.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
 
 @login_required
 def profile_edit(request):
-    profile = Profile.objects.get(user=request.user)
-    if request.method == "POST":
-        form = ProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.save()
-            return redirect('profile')
-    else:
-        form = ProfileForm(instance=profile)
+    if request.user_agent.is_mobile:
+        profile = Profile.objects.get(user=request.user)
+        if request.method == "POST":
+            form = ProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.save()
+                return redirect('profile')
+        else:
+            form = ProfileForm(instance=profile)
 
-    return render(request, 'mainsite/profile/proflie_edit.html', {'form': form})
+        return render(request, 'mainsite/profile/proflie_edit.html', {'form': form})
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
 
 @login_required
 def password_edit(request):
-    profile = Profile.objects.get(user=request.user)
-    if request.method == "POST":
-        form = ProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.save()
-            return redirect('profile')
+    if request.user_agent.is_mobile:
+        profile = Profile.objects.get(user=request.user)
+        if request.method == "POST":
+            form = ProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.save()
+                return redirect('profile')
+        else:
+            form = ProfileForm(instance=profile)
+
+        return render(request, 'mainsite/profile/proflie_edit.html', {'form': form})
     else:
-        form = ProfileForm(instance=profile)
+        return render(request, 'mainsite/PhonePage/index.html', )
 
-    return render(request, 'mainsite/profile/proflie_edit.html', {'form': form})
-
-# @login_required
-# def password_edit(request):
-#     profile = Profile.objects.get(user = request.user)
-#     if request.method == "POST":
-#         form = CreateUserForm(request.POST, instance=profile)
-#         if form.is_valid():
-#             profile = form.save(commit=False)
-#             profile.save()
-#             return redirect('profile')
-#     else:
-#         form = ProfileForm(instance=profile)
-
-    # return render(request, 'mainsite/profile/password_change.html', {'form': form})
 
 @login_required
 def password_change_done(request):
@@ -165,38 +203,60 @@ def password_change_done(request):
 
 
 def aboutkazan_view(request):
-    # question = get_object_or_404(Faq, pk=pk)
-    question = Faq.objects.order_by()[:5]
-    context = {
-        # 'question': question,
-        'question': question,
-    }
-    return render(request, 'mainsite/home/aboutKazan.html', context)    
+    if not request.user_agent.is_mobile:
+        # question = get_object_or_404(Faq, pk=pk)
+        question = Faq.objects.order_by()[:5]
+        context = {
+            # 'question': question,
+            'question': question,
+        }
+        return render(request, 'mainsite/home/aboutKazan.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
 def news_list_view(request):
-    news = Post.objects.all()
+    if request.user_agent.is_mobile:
+        news = Post.objects.all()
 
-    context = {
-        'news': news
-    }
-    return render(request, 'mainsite/news/news_list.html', context)
+        context = {
+            'news': news
+        }
+        return render(request, 'mainsite/news/news_list.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
 
 def news_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    last_4 = Post.objects.order_by()[:4]
-    context = {
-        'post': post,
-        'last_4': last_4,
-    }
-    return render(request, 'mainsite/news/news.html', context)
+    if request.user_agent.is_mobile:
+        post = get_object_or_404(Post, pk=pk)
+        last_4 = Post.objects.order_by()[:4]
+        context = {
+            'post': post,
+            'last_4': last_4,
+        }
+        return render(request, 'mainsite/news/news.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
 
+
+
+    # if request.user_agent.is_mobile:
+    #     return render(request, 'mainsite/privacyPolicy/PrivacyPolicy.html')
+    # else:
+    #     return render(request, 'mainsite/PhonePage/index.html', )
 
 def speciality_view(request, key):
-    speciality = get_object_or_404(Speciality, key=key)
-    context = {
-        'speciality':  speciality
 
-    }
-    return render(request, 'mainsite/incomingIMO/incomingIMO.html', context)
+    if request.user_agent.is_mobile:
+        speciality = get_object_or_404(Speciality, key=key)
+        context = {
+            'speciality': speciality
+
+        }
+        return render(request, 'mainsite/incomingIMO/incomingIMO.html', context)
+    else:
+        return render(request, 'mainsite/PhonePage/index.html', )
+
 
