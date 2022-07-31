3870c26036c60e1e226d32aa94e126d5f9e3db91
NodirRas <61974230+Nodir2001@users.noreply.github.com>
2022-07-31 15:10:05 +0300

    Update settings.py

--- imo/settings.py
+++ imo/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'imo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imodb',
+        'USER': 'imo',
+        'PASSWORD': 'imoproject',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
