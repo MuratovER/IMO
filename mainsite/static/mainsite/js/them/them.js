b574c91527e09b5d5d91041e52a9b76f1e9d4884
timur <timur.zagid@bk.ru>
2022-09-11 02:14:57 +0300

    подключин нужные скрипны, протестировал их работу

---
+++ mainsite/static/mainsite/js/them/them.js
@@ -0,0 +1,15 @@
+$(document).ready(
+    function(){
+        //them : true = dark; false = light;
+        let but = $("#flexSwitchCheckDefault");
+        if(localStorage.getItem("them") == null){
+            localStorage.setItem("them","false");
+        }else{
+            
+        }
+        
+
+
+
+    }
+);
