4232887c6b2f99cee9e8edca1d9d0f47f469d95a
Nodir2001 <rasulovnd@stud.kai.ru>
2022-09-12 20:17:20 +0300

    fix settings

--- imo/settings.py
+++ imo/settings.py
@@ -86,14 +86,16 @@ WSGI_APPLICATION = 'imo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imodb',
+        'USER': 'imo',
+        'PASSWORD': 'imoproject',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
