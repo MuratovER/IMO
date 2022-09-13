ca0078319fd04b393a1f041a9fcfb5000dcea281
Nodir2001 <rasulovnd@stud.kai.ru>
2022-09-13 21:47:14 +0300

    checked it

--- mainsite/static/mainsite/js/registration/ChangeEay.js
+++ mainsite/static/mainsite/js/registration/ChangeEay.js
@@ -1,11 +1,10 @@
-
 function ChangeEay(eleminformationEay, obj){
     if(obj.getAttribute("type") == "password"){
         obj.setAttribute("type","text");
-        eleminformationEay.style.backgroundImage ="url('../img_main/openEye.svg')";
+        eleminformationEay.style.backgroundImage ="url('../../../static/mainsite/img/openEye.svg')";
     }
     else{
         obj.setAttribute("type","password");
-        eleminformationEay.style.backgroundImage ="url('../img_main/closeEye.svg')";
+        eleminformationEay.style.backgroundImage ="url('../../../static/mainsite/img/closeEye.svg')";
     }
 }
