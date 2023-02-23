c5ad5c3501ebf8d4ddae29621ffcd5c654a900f3
Eldar <cooperative.entr@gmail.com>
2023-02-23 19:40:17 +0300

    wip

--- celery_app.py
+++ celery_app.py
@@ -3,12 +3,9 @@ import os
 from celery import Celery
 from django.conf import settings
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imo.settings')
 
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
-
-app = Celery('service')
+app = Celery('imo')
 app.config_from_object('django.conf:settings')
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
